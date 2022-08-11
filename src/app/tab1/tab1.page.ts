e61e385544146b52578b7497be99d719b0091d25
Luiz Felipe Santana Dos Santos <luiz.santos811@etec.sp.gov.br>
2022-08-10 21:18:21 -0300

    variaveis e exibicao de array com ngfor

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
+import { IFilmes } from './../models/IFilmes.models';
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,52 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  titulo='CineApp';
 
+  listaVideos: IFilmes[] = [
+    {
+      nome:'XXX',
+      lancamento:'xx/xx/xxxx',
+      duracao:'xh xxm',
+      classificacao: 76,
+      cartaz: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOQAAADdCAMAAACc/C7aAAAAA1BMVEX/PYsJYBshAAAASElEQVR4nO3BgQAAAADDoPlTX+EAVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACcBsWxAAHcrYvnAAAAAElFTkSuQmCC',
+      generos: ['xy','xz','xx']
+    }
+  ];
+
+  constructor(public alertController: AlertController,
+              public toastController: ToastController) {}
+
+  async exibirAlerta() {
+    const alert = await this.alertController.create({
+      header: 'Favorito',
+      message: 'Deseja favoritar?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelado');
+          },
+        },
+        {
+          text: 'Sim',
+          handler: () => {
+            this.apresentarToast();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Filmes adicionado aos favoritos',
+      duration: 1500,
+      color: 'primary'
+    });
+    toast.present();
+  }
 }
